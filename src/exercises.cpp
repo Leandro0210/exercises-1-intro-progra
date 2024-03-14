d1f009ed3bd0f1808e10c91b24ecad7c0d057348
Leandro0210 <159178056+Leandro0210@users.noreply.github.com>
2024-03-13 20:06:15 -0400

    prueba 14

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -40,40 +40,8 @@ void exercise_5(char character) {
 }
 
 void exercise_6(int number) {
-  string nombreDia;
-
-    switch (number) {
-        case 1:
-            nombreDia = "Lunes";
-            break;
-        case 2:
-            nombreDia = "Martes";
-            break;
-        case 3:
-            nombreDia = "Miércoles";
-            break;
-        case 4:
-            nombreDia = "Jueves";
-            break;
-        case 5:
-            nombreDia = "Viernes";
-            break;
-        case 6:
-            nombreDia = "Sábado";
-            break;
-        case 7:
-            nombreDia = "Domingo";
-            break;
-        default:
-            nombreDia = "Número de día inválido";
-            break;
-    }
-
- cout << "El nombre del día es: " << nombreDia << endl;
-
+  
 }
-
-
 
 void exercise_7(double r) {
   // TODO: YOUR CODE HERE
