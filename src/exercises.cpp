9682b491cc0da445c5ee531c24e6305863bccc68
Leandro0210 <159178056+Leandro0210@users.noreply.github.com>
2024-04-29 12:12:35 -0400

    76

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -240,6 +240,7 @@ string exercise_14(int number_of_docs) {
     } else {
         return to_string(number_of_docs) + " documentos encontrados";
     }
+    return "";
 }
 
 void exercise_15(int a, int b, int c) {
