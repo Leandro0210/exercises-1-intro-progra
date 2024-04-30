678a207c408214815176e4ac21cdfbfce445bad3
Leandro0210 <159178056+Leandro0210@users.noreply.github.com>
2024-04-30 09:36:03 -0400

    87

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -173,7 +173,7 @@ void exercise_8(long int seconds) {
         }
 }
 
-void exercise_9(string s1, string s2, string s3, string s4, string s5) {
+string exercise_9(string s1, string s2, string s3, string s4, string s5) {
   string re;
 
   if(s1[0] == s5[0]) {
